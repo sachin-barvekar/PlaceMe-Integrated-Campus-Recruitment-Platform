b869fd12af5d7b6cd4973dcea7e46d6c6740c936
sachin-barvekar <sachinbarvekar2003@gmail.com>
2025-02-24 16:12:55 +0530

    authcontext referesh token type remove.

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -7,7 +7,6 @@ interface AuthContextType {
   loading: boolean;
   login: () => Promise<void>;
   logout: () => Promise<void>;
-  refreshToken: () => void;
   role: string | null;
   setRole: (role: string | null) => void;
 }
